b9f6fcb34bc74091e59bb3249cdb0674972c50f5
M.C. van den Boogaart <scola@kvdb.name>
2016-03-17 19:38:20 +0100

    Make watch and reload optional

---
+++ helper/reload.js
@@ -0,0 +1,4 @@
+const element = document.createElement('script');
+element.type = 'text/javascript';
+element.src = '//' + location.hostname + ':35729/livereload.js?snipver=1';
+document.body.appendChild(element);
